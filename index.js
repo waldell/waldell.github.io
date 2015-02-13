d3bdf71e82509abc7ef32211f7e8817c18c5ff33
Oscar Johansson <Kodninja@Oscars-Mac-mini.local>
2015-02-13 23:25:40 +0100

    google tag manager test

--- index.js
+++ index.js
@@ -3,16 +3,31 @@
     'use strict';
 
     function createPager() {
-        var sections, pager, pagerItem, i;
+        var sections, pager, pagerItems, pagerItem, i, j;
 
         sections    = document.querySelectorAll('section');
         pager       = document.querySelector('#pager');
+        pagerItems  = [];
+
+        function click(e) {
+            var self = e.currentTarget;
+            for (j = 0; j < pagerItems.length; j++) {
+                pagerItems[j].classList.remove('active');
+            }
+            self.classList.add('active');
+        }
 
         for (i = 0; i < sections.length; i++) {
             pagerItem = document.createElement('a');
             pagerItem.innerHTML = '.';
             pagerItem.href = '#' + sections[i].id;
+            pagerItem.addEventListener('click', click);
             pager.appendChild(pagerItem);
+        }
+
+        pagerItems = pager.querySelectorAll('a');
+        if (pagerItems.length > 0) {
+            pagerItems[0].click();
         }
     }
 
@@ -20,7 +35,7 @@
         createPager();
     }
 
-    document.addEventListener("DOMContentLoaded", function (e) {
+    document.addEventListener('DOMContentLoaded', function (e) {
         init();
     });
 
