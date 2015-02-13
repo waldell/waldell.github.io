626501115332f55f07e7decce6cb6fc0a47e889a
Oscar Johansson <Kodninja@Oscars-Mac-mini.local>
2015-02-14 00:42:20 +0100

    ga touches

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*jslint plusplus: true */
 (function () {
     'use strict';
-
+    var ga;
     function createPager() {
         var sections, pager, pagerItems, pagerItem, i, j;
 
@@ -33,19 +33,18 @@
         }
     }
     function createGa() {
-        (function(i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r;
-            i[r]=i[r] || function () {
-                (i[r].q = i[r].q || [])
-                    .push(arguments)
-            },
-            i[r].l = 1 * new Date();
-            a = s.createElement(o),
+        (function (i, s, o, g, r, a, m) {
+            i.GoogleAnalyticsObject = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments);
+            };
+            i[r].l = new Date().getTime();
+            a = s.createElement(o);
             m = s.getElementsByTagName(o)[0];
             a.async = 1;
             a.src = g;
-            m.parentNode.insertBefore(a,m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+            m.parentNode.insertBefore(a, m);
+        }(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga'));
         ga('create', 'UA-59751543-1', 'auto');
         ga('send', 'pageview');
     }
