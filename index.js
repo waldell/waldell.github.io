6795b01e7e03d065d52873aa2526edc26cf50ac9
Oscar Johansson <Kodninja@Oscars-Mac-mini.local>
2015-02-14 00:02:21 +0100

    ga

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /*jslint plusplus: true */
 (function () {
     'use strict';
-    var _gaq = _gaq || [];
 
     function createPager() {
         var sections, pager, pagerItems, pagerItem, i, j;
@@ -16,7 +15,10 @@
                 pagerItems[j].classList.remove('active');
             }
             self.classList.add('active');
-            _gaq.push(['_trackEvent', 'Section', self.href]);
+
+            ga('send', 'pageview', {
+                'page': self.href
+            });
         }
 
         for (i = 0; i < sections.length; i++) {
@@ -33,37 +35,21 @@
         }
     }
     function createGa() {
-//        (function(i, s, o, g, r, a, m) {
-//            i['GoogleAnalyticsObject'] = r;
-//            i[r]=i[r] || function () {
-//                (i[r].q = i[r].q || [])
-//                    .push(arguments)
-//            },
-//            i[r].l = 1 * new Date();
-//            a = s.createElement(o),
-//            m = s.getElementsByTagName(o)[0];
-//            a.async = 1;
-//            a.src = g;
-//            m.parentNode.insertBefore(a,m)
-//        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-//        ga('create', 'UA-59751543-1', 'auto');
-//        ga('send', 'pageview');
-
-
-        _gaq.push(['_setAccount', 'UA-59751543-1']);
-        _gaq.push(['_trackPageview']);
-
-        (function () {
-            var ga, s;
-
-            ga = document.createElement('script');
-            s = document.getElementsByTagName('script')[0];
-
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(ga, s);
-        }());
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r]=i[r] || function () {
+                (i[r].q = i[r].q || [])
+                    .push(arguments)
+            },
+            i[r].l = 1 * new Date();
+            a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a,m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-59751543-1', 'auto');
+        ga('send', 'pageview');
     }
 
     function init() {
